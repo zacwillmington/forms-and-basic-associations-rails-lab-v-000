d60bf8c43831d7a10b18c543b46038a8a85cc4f9
Zac Willmington <zac.willmington@gmail.com>
2018-07-11 16:54:03 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -49,6 +49,6 @@ class SongsController < ApplicationController
 
   def song_params
       binding.pry
-    params.require(:song).permit(:title, :artist_name, :genre_id, :note_contents => params['song']['notes'])
+    params.require(:song).permit(:title, :artist_name, :genre_id, :note_contents => [])
   end
 end
